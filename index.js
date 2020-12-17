0c0bddf2e73b48e3e936af584b78e9dd2388d1c8
Lena Hou <lenahou2013@gmail.com>
2020-12-17 18:41:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ function placeOrder(cardNumber) {
   // write your code here
   var emptyArray = [];
   if(cardNumber) {
-    return `Your total cost is$${total()}, which will be charged to the card ${cardNumber}.`;
+    return `Your total cost is$ ${total()}, which will be charged to the card ${cardNumber}.`;
     setCart(emptyArray);
   } else {
     return `Sorry, we don't have a credit card on file for you.`;
