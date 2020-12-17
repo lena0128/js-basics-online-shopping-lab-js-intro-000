115e7d2741511c0d92e2791f590566dcc1c77b99
Lena Hou <lenahou2013@gmail.com>
2020-12-17 18:48:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ function placeOrder(cardNumber) {
   total();
   if(cardNumber) {
     cart.length = 0;
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+    return `Your total cost is $${myTotal}, which will be charged to the card ${cardNumber}.`;
   } else {
     return `Sorry, we don't have a credit card on file for you.`;
   }
