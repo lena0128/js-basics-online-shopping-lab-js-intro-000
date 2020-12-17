9431fb084221d9d3ee1ad78fb248d444023ea53d
Lena Hou <lenahou2013@gmail.com>
2020-12-17 18:46:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -67,8 +67,8 @@ return "That item is not in your cart.";
 function placeOrder(cardNumber) {
   // write your code here
   if(cardNumber) {
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
     cart.length = 0;
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   } else {
     return `Sorry, we don't have a credit card on file for you.`;
   }
